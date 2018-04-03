6db7b6b0a4ebfebf4c8bff3d1a4236739f479a61
Michael Joyner <joyner.michael@gmail.com>
2018-04-03 10:13:16 +0800

    use mysql database in prod

--- knexfile.js
+++ knexfile.js
@@ -37,9 +37,13 @@ module.exports = {
   },
 
   production: {
-    client: "sqlite3",
+    client: "mysql",
     connection: {
-      filename: "./dev.sqlite3"
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
     },
     migrations: {
       tableName: "knex_migrations"
