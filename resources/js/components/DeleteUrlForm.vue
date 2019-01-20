337e6c801388f0d30afa6cbfc996c9c10ff98e09
Michael Joyner <joyner.michael@gmail.com>
2019-01-20 17:59:14 +0800

    add ui to delete url

---
+++ resources/js/components/DeleteUrlForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class>
+    <form action @submit.prevent="deleteUrl">
+      <p>Are you sure you would like to stop monitoring this URL?</p>
+      <div class="mt-8 flex justify-end p-4">
+        <button
+          class="shadow px-4 py-2 mx-4 rounded bg-grey-light"
+          type="button"
+          @click="cancel"
+        >Cancel</button>
+        <button class="shadow px-4 py-2 mx-4 rounded bg-red text-white" type="submit">Do it!</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: ["urlId"],
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+
+    deleteUrl() {
+      axios
+        .delete(`/urls/${this.urlId}`)
+        .then(() => this.$emit("url-deleted"))
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
--- resources/js/components/UrlPage.vue
+++ resources/js/components/UrlPage.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
-    <div class="m-4 p-4 shadow">
-      <p class="mb-1 text-xs text-green font-bold">Page</p>
-      <p v-show="url.url">{{ url.url }}</p>
+    <div class="m-4 p-4 shadow flex justify-between">
+      <div>
+        <p class="mb-1 text-xs text-green font-bold">Page</p>
+        <p v-show="url.url">{{ url.url }}</p>
+      </div>
+      <div>
+        <button @click="showDeleteForm = true">
+          <svg
+            class="text-red"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              class="current-fill"
+              d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"
+            ></path>
+          </svg>
+        </button>
+        <modal :show="showDeleteForm" @close="showDeleteForm = false" class="rounded">
+          <div class="max-w-sm mx-auto p-4 border-t-4 border-red rounded">
+            <delete-url-form
+              :urlId="url.id"
+              @cancel="showDeleteForm = false"
+              @url-deleted="navigateHome"
+            ></delete-url-form>
+          </div>
+        </modal>
+      </div>
     </div>
-      <div class="m-4 p-4 shadow">
+    <div class="m-4 p-4 shadow">
       <div class="flex justify-between">
-        <div class="">
+        <div class>
           <p class="text-xs text-green-light font-bold mb-1">Success</p>
           <p class="text-xl font-black text-green">{{ summary_pass }}</p>
         </div>
-        <div class="">
+        <div class>
           <p class="text-xs text-red-light font-bold mb-1">Fail</p>
           <p class="text-xl font-black text-red">{{ summary_fail }}</p>
         </div>
-        <div class="">
+        <div class>
           <p class="text-xs text-blue-light font-bold mb-1">Score</p>
           <p class="text-xl font-black text-blue">{{ summary_percent }}%</p>
         </div>
@@ -23,19 +50,30 @@
     <div class="m-4 p-4 shadow">
       <p class="mb-1 text-xs text-green font-bold">Recent checks</p>
       <div class="flex flex-wrap">
-          <div class="check rounded-full w-4 h-4 m-2" :class="{'bg-green': checkIsOkay(check), 'bg-red': !checkIsOkay(check)}" v-for="check in checks" :key="check.created_at">
-          </div>
+        <div
+          class="check rounded-full w-4 h-4 m-2"
+          :class="{'bg-green': checkIsOkay(check), 'bg-red': !checkIsOkay(check)}"
+          v-for="check in checks"
+          :key="check.created_at"
+        ></div>
       </div>
-    </div>  
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Modal from "@dymantic/modal";
+import DeleteUrlForm from "./DeleteUrlForm.vue";
 export default {
+  components: {
+    Modal,
+    DeleteUrlForm
+  },
   data() {
     return {
-      url: { url: false }
+      url: { url: false },
+      showDeleteForm: false
     };
   },
 
@@ -61,8 +99,7 @@ export default {
     summary_percent() {
       if (this.has_summary && this.summary_pass + this.summary_fail !== 0) {
         return (
-          this.summary_pass /
-          (this.summary_pass + this.summary_fail) *
+          (this.summary_pass / (this.summary_pass + this.summary_fail)) *
           100
         ).toPrecision(3);
       }
@@ -90,6 +127,11 @@ export default {
   methods: {
     checkIsOkay(check) {
       return check.status / 100 < 4;
+    },
+
+    navigateHome() {
+      console.log("hererere");
+      this.$router.push("/url-list");
     }
   }
 };
